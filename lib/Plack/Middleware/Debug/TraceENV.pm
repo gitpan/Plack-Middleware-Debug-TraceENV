acf38d7b75050a7754119bcaf7b3fde24646fdf2
Dai Okabayashi <bayashi@cpan.org>
2013-03-03 02:59:23 +0000

    Import of BAYASHI/Plack-Middleware-Debug-TraceENV-0.041 from CPAN.
    gitpan-cpan-distribution: Plack-Middleware-Debug-TraceENV
    gitpan-cpan-version:      0.041
    gitpan-cpan-path:         BAYASHI/Plack-Middleware-Debug-TraceENV-0.041.tar.gz
    gitpan-cpan-author:       BAYASHI
    gitpan-cpan-maturity:     released

--- lib/Plack/Middleware/Debug/TraceENV.pm
+++ lib/Plack/Middleware/Debug/TraceENV.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use Plack::Util::Accessor qw/method/;
 use parent qw/Plack::Middleware::Debug::Base/;
-our $VERSION = '0.04';
+our $VERSION = '0.041';
 
 my $ENABLE = +{};
 
